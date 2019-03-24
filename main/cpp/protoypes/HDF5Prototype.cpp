b123d2f7c67d9ec61eb2057da6fc345871fb4d5a
Benjamin-Vandersmissen <benjamin.vandersmissen@student.uantwerpen.be>
2019-03-24 11:29:48 +0100

    Added some files I forgot for building the C++ version of the HDF5 library. Also added a small prototype, to be polished a little more

--- main/cpp/protoypes/HDF5Prototype.cpp
+++ main/cpp/protoypes/HDF5Prototype.cpp
@@ -3,8 +3,21 @@
 //
 
 #include <H5Cpp.h>
+#include <string>
+
+using namespace H5;
 
 int main(){
+    H5File file("hdf5_test.h5", H5F_ACC_TRUNC);
+    hsize_t dimensions[2];
+    dimensions[0] = 3;
+    dimensions[1] = 3;
+    int rank = 2; // matrix van 3x3 opslaan (rank = # dimensies)
+    DataSpace dataSpace(rank, dimensions);
 
+    StrType variable_length_string_type(0, H5T_VARIABLE);
+
+    DataSet dataSet = file.createDataSet("test", variable_length_string_type, dataSpace);
+    file.close();
     return 0;
 }
