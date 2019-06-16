7cd8c5afc4d74d28283af388968405c69e7cad11
larsvanroy <s0161083@ad.ua.ac.be>
2019-06-16 11:39:59 +0200

    fixed test parameters

--- test/cpp/gtester/ScenarioData.cpp
+++ test/cpp/gtester/ScenarioData.cpp
@@ -56,7 +56,7 @@ namespace Tests {
         const map<string, unsigned int> targets_geopop = {
                 {"influenza_a_geopop", 553000U}, {"influenza_b_geopop", 0U},     {"influenza_c_geopop", 5U},
                 {"measles_16_geopop", 270000U},  {"measles_26_geopop", 600000U}, {"r0_0_geopop", 1200U},
-                {"r0_12_geopop", 37000U},        {"r0_16_geopop", 69000U},       {"r0_4_geopop", 4300U},
+                {"r0_12_geopop", 37000U},        {"r0_16_geopop", 65000U},       {"r0_4_geopop", 4300U},
                 {"r0_8_geopop", 15000U}};
 
         const map<string, double> margins_geopop = {{"influenza_a_geopop", 3.0e-03}, {"influenza_b_geopop", 0.0},
