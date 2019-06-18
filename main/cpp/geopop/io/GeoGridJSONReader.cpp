af352411944dab6c4b6d5135cbde6c6eac5ddd65
EvelienDaems <s0161790@ad.ua.ac.be>
2019-06-18 17:52:35 +0200

    checking first char of file?

--- main/cpp/geopop/io/GeoGridJSONReader.cpp
+++ main/cpp/geopop/io/GeoGridJSONReader.cpp
@@ -41,10 +41,12 @@ void GeoGridJSONReader::Read()
         json json_file;
 
         try {
+                char blub[5];
+                m_inputStream->getline(blub, 256);
+                std::cout << blub << std::endl;
                 json_file = json::parse(*m_inputStream);
 
         } catch (json::parse_error& e) {
-                std::cout << m_inputStream->rdbuf() << std::endl;
                 std::cout << "Here is the magic error you receive on mac: " << e.what() << std::endl;
                 throw Exception("Problem parsing JSON file, check whether empty or invalid JSON.");
         }
